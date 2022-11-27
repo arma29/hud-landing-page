5b36d16937b7e9e27f4d787d74780daf1b6f3d9b
Arnaldo Andrade <arnaldo.andrade@serttel.com.br>
2022-11-27 19:36:00 -0300

    Add success stories section

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const myNav = document.getElementsByTagName('nav')[0];
 
-window.onscroll = function() {
-    "use strict";
-    const limit = 100;
-    if (document.body.scrollTop >= limit || document.documentElement.scrollTop >= limit) {
-      myNav.classList.add("js-navbar-bg");
-    } else {
-      myNav.classList.remove("js-navbar-bg");
-    }
-  };
+function handleNavbarBg() {
+  "use strict";
+  const limit = 100;
+  if (document.body.scrollTop >= limit || document.documentElement.scrollTop >= limit) {
+    myNav.classList.add("js-navbar-bg");
+  } else {
+    myNav.classList.remove("js-navbar-bg");
+  }
+};
+
+window.onscroll = handleNavbarBg
+
+handleNavbarBg();
+
+
+
